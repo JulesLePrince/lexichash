f29579b2920bd07adf0641d65431cc1e9c7add70
Jules Le Prince <juleslp35740@gmail.com>
2026-06-17 16:45:52 +0200

    Buggy iterators

--- src/builder/single_thread.rs
+++ src/builder/single_thread.rs
@@ -1,3 +1,5 @@
+use crate::builder::iter::SuffixIter;
+
 use super::iter::PrefixIter;
 use super::utils::*;
 
@@ -18,12 +20,17 @@ impl<'a> SingleThreadBuilder<'a> {
     }
 
     pub fn get_prefix_iterator(&self, packed_bytes: &'a [u128]) -> PrefixIter<'a> {
-        PrefixIter::new(self.prefix_size, packed_bytes)
+        PrefixIter::new(self.prefix_size, self.suffix_size, packed_bytes)
     }
 
+    pub fn get_suffix_iterator(&self, packed_bytes: &'a [u128]) -> SuffixIter<'a> {
+        SuffixIter::new(self.prefix_size, self.suffix_size, packed_bytes)
+    }
+
+
     pub fn build(&self, packed_bytes: &'a [u128]) {
-        for kmer in self.get_prefix_iterator(packed_bytes) {
-            let seq_as_string = packed_to_string(kmer, self.prefix_size);
+        for kmer in self.get_suffix_iterator(packed_bytes) {
+            let seq_as_string = packed_to_string(kmer, self.suffix_size);
             println!("{seq_as_string}");
         }
     }
