acf82cf0fdff882b773e2bb4758f519bc8a9ad05
Jules Le Prince <juleslp35740@gmail.com>
2026-06-17 18:09:57 +0200

    generic kmer iterators

--- src/builder/iter.rs
+++ src/builder/iter.rs
@@ -2,99 +2,61 @@ use super::utils::get_u64_unaligned;
 use bytemuck::cast_slice;
 
 /// Iterate `packed_bytes` over factors of size `prefix_size`
-pub struct PrefixIter<'a> {
+pub struct KmerIterator<'a> {
     packed_u32_bytes: &'a [u32],
     pos: usize,
-    prefix_size: usize,
+    size: usize,
     current_u64_window: u64,
-    remaining_inside_window: usize,
     shift_inside_window: usize,
     filter_prefix_mask: u32,
 }
 
-impl<'a> PrefixIter<'a> {
-    pub fn new(prefix_size: usize, suffix_size: usize, packed_bytes: &'a [u128]) -> Self {
+impl<'a> KmerIterator<'a> {
+    pub fn new(size: usize, packed_bytes: &'a [u128]) -> Self {
         let packed_u32_bytes = cast_slice(packed_bytes);
         Self {
-            packed_u32_bytes,
+            packed_u32_bytes: packed_u32_bytes,
             pos: 0,
-            prefix_size,
-            current_u64_window: get_u64_unaligned(packed_u32_bytes, suffix_size),
-            remaining_inside_window: 17 - suffix_size,
-            shift_inside_window: suffix_size,
-            filter_prefix_mask: u32::MAX >> (32 - 2 * prefix_size),
+            size: size,
+            current_u64_window: get_u64_unaligned(packed_u32_bytes, 0),
+            shift_inside_window: 0,
+            filter_prefix_mask: u32::MAX >> (32 - 2 * size),
         }
     }
 }
 
-impl<'a> Iterator for PrefixIter<'a> {
+impl<'a> Iterator for KmerIterator<'a> {
     type Item = u32;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.remaining_inside_window == 0 {
+        // If we reached the end of the current u64
+        if self.shift_inside_window >= 32 {
+            // End of the sequence
             if self.pos == self.packed_u32_bytes.len() - 2 {
                 return None;
             }
 
             self.pos += 1;
             self.current_u64_window = get_u64_unaligned(self.packed_u32_bytes, self.pos);
-            self.remaining_inside_window = 17;
             self.shift_inside_window = 0;
         }
 
         let current = ((self.current_u64_window >> self.shift_inside_window) as u32)
             & self.filter_prefix_mask;
         self.shift_inside_window += 2;
-        self.remaining_inside_window -= 1;
         Some(current)
     }
 }
 
-pub struct SuffixIter<'a> {
-    packed_u32_bytes: &'a [u32],
-    pos: usize,
-    suffix_size: usize,
-    current_u64_window: u64,
-    remaining_inside_window: usize,
-    shift_inside_window: usize,
-    filter_suffix_mask: u32,
-}
-
-
-impl<'a> SuffixIter<'a> {
-    pub fn new(prefix_size: usize, suffix_size: usize, packed_bytes: &'a [u128]) -> Self {
-        let packed_u32_bytes = cast_slice(packed_bytes);
-        Self {
-            packed_u32_bytes,
-            pos: 0,
-            suffix_size,
-            current_u64_window: get_u64_unaligned(packed_u32_bytes, 0),
-            remaining_inside_window: 17,
-            shift_inside_window: 0,
-            filter_suffix_mask: u32::MAX >> (32 - 2 * suffix_size),
-        }
-    }
-}
-
-impl<'a> Iterator for SuffixIter<'a> {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.remaining_inside_window == 0 {
-            if self.pos == self.packed_u32_bytes.len() - 2 {
-                return None;
-            }
-
-            self.pos += 1;
-            self.current_u64_window = get_u64_unaligned(self.packed_u32_bytes, self.pos);
-            self.remaining_inside_window = 17;
-            self.shift_inside_window = 0;
-        }
-
-        let current = ((self.current_u64_window >> self.shift_inside_window) as u32)
-            & self.filter_suffix_mask;
-        self.shift_inside_window += 2;
-        self.remaining_inside_window -= 1;
-        Some(current)
-    }
-}
 
 // TODO add tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prefix_iterator() {
+        // Generate random_seq
+        let seq: &[u32] = &[0xDEADBEEF; 1024];
+
+    }
+}
--- src/builder/single_thread.rs
+++ src/builder/single_thread.rs
@@ -1,6 +1,6 @@
-use crate::builder::iter::SuffixIter;
+use crate::builder::utils;
 
-use super::iter::PrefixIter;
+use super::iter::KmerIterator;
 use super::utils::*;
 
 /// Parameters for a sketch builder on one thread
@@ -19,19 +19,23 @@ impl<'a> SingleThreadBuilder<'a> {
         }
     }
 
-    pub fn get_prefix_iterator(&self, packed_bytes: &'a [u128]) -> PrefixIter<'a> {
-        PrefixIter::new(self.prefix_size, self.suffix_size, packed_bytes)
+    pub fn get_prefix_iterator(&self, packed_bytes: &'a [u128]) -> KmerIterator<'a> {
+        KmerIterator::new(self.prefix_size, packed_bytes)
     }
 
-    pub fn get_suffix_iterator(&self, packed_bytes: &'a [u128]) -> SuffixIter<'a> {
-        SuffixIter::new(self.prefix_size, self.suffix_size, packed_bytes)
+    pub fn get_suffix_iterator(&self, packed_bytes: &'a [u128]) -> KmerIterator<'a> {
+        KmerIterator::new(self.suffix_size, packed_bytes)
     }
 
 
     pub fn build(&self, packed_bytes: &'a [u128]) {
-        for kmer in self.get_suffix_iterator(packed_bytes) {
-            let seq_as_string = packed_to_string(kmer, self.suffix_size);
-            println!("{seq_as_string}");
+        let mut prefix_iterator = self.get_prefix_iterator(packed_bytes);
+        let mut suffix_iterator = self.get_suffix_iterator(packed_bytes);
+        suffix_iterator.nth(self.prefix_size-1);
+        while let Some(suffix) = suffix_iterator.next() {
+            if let Some(prefix) = prefix_iterator.next() {
+                println!("{}{}", utils::packed_to_string(prefix, self.prefix_size), utils::packed_to_string(suffix, self.suffix_size))
+            }
         }
     }
 }
